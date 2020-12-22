98aca21c3fd958ea7bb2fa3ad2ff5bf5c2fc86cf
Gary Stafford <gary.a.stafford@gmail.com>
2020-12-22 14:25:16 -0500

    Prevent errors in bootstrap script

--- bootstrap_emr/bootstrap_actions.sh
+++ bootstrap_emr/bootstrap_actions.sh
@@ -11,4 +11,4 @@ aws configure set region \
   "$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)"
 
 # install some useful python packages
-sudo python3 -m pip install boto3 ec2-metadata scipy scikit-learn pandas awswrangler
+sudo python3 -m pip install boto3 ec2-metadata awswrangler
