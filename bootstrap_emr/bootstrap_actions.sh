d5fdf29ccc0ada5ff51b146144d3dd960c089bd2
Gary A. Stafford <garystafford@rochester.rr.com>
2021-04-05 11:49:55 -0400

    Fix failure from awswrangler

--- bootstrap_emr/bootstrap_actions.sh
+++ bootstrap_emr/bootstrap_actions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Purpose: EMR bootstrap script
-# Author:  Gary A. Stafford (December 2020)
+# Author:  Gary A. Stafford (2021-04-05)
 
 # update and install some useful yum packages
 sudo yum install -y jq
@@ -11,4 +11,4 @@ aws configure set region \
   "$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)"
 
 # install some useful python packages
-sudo python3 -m pip install boto3 ec2-metadata awswrangler
+sudo python3 -m pip install boto3 ec2-metadata
